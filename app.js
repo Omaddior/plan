16f20c2bda2d210ac66a48578d3d93c6166010d8
Omaddior <omaddiyorumarov@gmail.com>
2025-09-25 08:32:03 +0900

    feat: Task B has been done and uploaded

---
+++ app.js
@@ -0,0 +1,70 @@
+console.log("Web Serverni boshlash");
+const { error } = require("console");
+const express = require("express");
+const app = express();
+
+
+
+// MongoDB ni chaqirish
+const db = require("./server").db();
+
+
+//1. KIRISH KODLARI
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err){
+        console.log("ERROR")
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+//2. Sessions: SEssion Codes
+
+//3. Views ga bog'liq kodlar
+app.set("views", "views");
+
+app.set("view engine", "ejs");
+
+//4 Routing Kodlari:
+app.post("/create-item", (req, res) => {console.log(req.body);
+res.end("success");
+console.log('user entered /');
+const new_reja = req.body.reja;
+db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+    if(err){
+        console.log(err);
+        res.end("Something went wrong");
+    }
+    else{
+        res.end("Successfully added"); 
+    }
+});
+
+
+});
+// app.get("/hello", function(req, res) {
+//     res.end(`<h1>GOOD MORNING</h1>`);
+// } );
+// app.get('/author', (req, res) => {
+//     res.render("develope", {user: user});
+// });
+app.get("/",function(req, res){
+    console.log('user entered /');
+    db.collection("plans").find().toArray((err, data) =>{
+        if(err) {
+            console.log(err);
+            res.end("Something went wrong!!!");
+        } else {
+            console.log(data);
+            res.render("reja", {items: data}); 
+        }
+    });
+});
+
+
+module.exports = app;
--- train.js
+++ train.js
@@ -1,1 +1,91 @@
-console.log("Train Task is running");
+// console.log("Jeck  Ma Maslahatlari: ");
+// const list = [
+//     "Be a good student", //0-20
+//     "Choose a right boss and make many mistakes", //20 - 30
+//     "Start working on yourself", // 30 - 40
+//     "Do what you are good at", // 40 - 50
+//     "Invest in young generation", // 50 - 60
+//     "Enjoy your life and it is all useless then", //60 ~
+// ];
+// function giveMeAdvice (age, callback){
+//     if(typeof age !== 'number') callback ("Please enter your age", null)
+//         else if (age <=20) callback(null, list[0]);
+//         else if (age >20 && age<=30) callback(null, list[1]);
+//         else if (age >30 && age<=40) callback(null, list[2]);
+//         else if (age >40 && age<=50) setTimeout(function() {callback(null, list[3]);}, 5000);
+//         else if (age >50 && age <=60) callback(null, list[4]);
+//         else {
+//             setInterval(() => {
+//                 callback(null, list[5]);
+//             }, 1000);
+//         }
+// }
+// console.log("Started Here");
+// giveMeAdvice(67, (err, data) => {
+//     if(err) console.log('ERROR: ', err);
+//     console.log(data);
+// }); 
+// console.log("Passed Here");
+
+// async function giveMeAdvice (age){
+//     if(typeof age !== 'number') throw new Error("Please insert a number identifying your real age");
+//         else if (age <=20) return list[0];
+//         else if (age >20 && age<=30) return list[1];
+//         else if (age >30 && age<=40) return list[2];
+//         else if (age >40 && age<=50) return list[3];
+//         else if (age >50 && age <=60) return list[4];
+//         else {
+//             return new Promise ((resolve, reject) => setTimeout(() => resolve (list[5])), 5000);}
+//         }
+        
+//         async function run () {
+//             let answer = await giveMeAdvice(66);
+//             console.log("Javob-1: ", answer);
+//             answer = await giveMeAdvice(54); 
+//             console.log("Javob-2: ", answer);
+//             answer = await giveMeAdvice(24);
+//             console.log("Javob: ", answer);
+//         }
+//         run();
+
+
+
+//TASK A
+// function countLetter(l, word) {
+//     let sum = 0;                           // har safar yangi hisob
+//     let newMassive = Array.from(word);     // har safar yangi massiv
+//     let length = newMassive.length;
+
+//     for (let i = 0; i < length; i++) {
+//         if (newMassive[i] === l) {         // taqqoslash ===
+//             sum++;
+//         }
+//     }
+//     return sum;
+// }
+
+// // Testlar
+// console.log(countLetter('m', 'omaddior')); // 1
+// console.log(countLetter('m', 'manzara'));  // 1
+// console.log(countLetter('j', 'banana'));   // 0
+// console.log(countLetter('a', 'banana'));   // 3
+// console.log(countLetter('m', 'Mirzakarimov Muhammadrahim'));
+
+//Task B
+
+function countDigit (word){
+    let sum = 0;
+    for (let i = 0; i<word.length; i++){
+        const eachElement = word[i];
+        if(eachElement >=0 && eachElement <=9)
+        {
+            console.log("Qiymat: ", word[i]);
+            sum++;
+        }
+
+    }
+    console.log(`${word} sozi tarkibida ${sum} ta raqam mavjud...`);
+};
+countDigit("sana123");
+countDigit("j4n5j34njk5n43jk5n4jk");
+
