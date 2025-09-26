1261746778af05a552f7920aaede9f0b71f00b2c
Omaddior <omaddiyorumarov@gmail.com>
2025-09-26 23:37:13 +0900

    feat: author ejs page has been created

---
+++ app.js
@@ -0,0 +1,68 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const res = require("express/lib/response");
+
+
+
+
+
+// MongoDB ni chaqirish
+const db = require("./server").db();
+
+//1. KIRISH KODLARI
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err){
+        console.log("ERROR")
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+//2. Sessions: SEssion Codes
+
+//3. Views ga bog'liq kodlar
+app.set("views", "views");
+
+app.set("view engine", "ejs");
+
+//4 Routing Kodlari:
+app.post("/create-item", (req, res) => {console.log(req.body);
+console.log('user entered /');
+const new_reja = req.body.reja;
+db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+    if(err){
+        console.log(err);
+        res.end("Something went wrong");
+    }
+    else{
+        res.end("Successfully added"); 
+    }
+});
+
+
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+});
+app.get("/",function(req, res){
+    console.log('user entered /');
+    db.collection("plans").find().toArray((err, data) =>{
+        if(err) {
+            console.log(err);
+            res.end("Something went wrong!!!");
+        } else {
+            console.log(data);
+            res.render("reja", {items: data}); 
+        }
+    });
+});
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require('http');
-//1. KIRISH KODLARI
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+const mongodb = require("mongodb");
 
-//2. Sessions: SEssion Codes
-
-//3. Views ga bog'liq kodlar
-app.set("views", "views");
-
-app.set("view engine", "ejs");
-
-//4 Routing Kodlari:
-app.post("/create-item", (req, res) => {console.log(req.body);
-res.json({test: "success"});
-
-});
-app.get("/hello", function(req, res) {
-    res.end(`<h1>GOOD MORNING</h1>`);
-} );
-app.get("/",function(req, res){
-    res.render("harid");
-});
-
-
-const server = http.createServer(app);
+let db;
+const connectionString = "mongodb+srv://omaddiyorumarov:FvXBrCwOHOS1NTyh@cluster0.spwuzp4.mongodb.net/";
+mongodb.connect(connectionString, {
+useNewUrlParser: true, 
+useUnifiedTopology: true,
+}, (err, client) => {
+if(err) console.log("Error: MongoDB connection");
+else{
+    console.log("MongoDB connection is successful");
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function (req, res){
-    console.log(`The server is running on the port ${PORT}`);
+server.listen(PORT, function (){
+    console.log(`The server is running on the port ${PORT}, https://localhost${PORT}`);
+});
+}
 });
